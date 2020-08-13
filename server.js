678b43300a068ecc77725235c6cb7efbcb38cb63
smooney211993 <sm.athome21@gmail.com>
2020-08-13 11:35:22 +1000

    deleted cors header

--- server.js
+++ server.js
@@ -20,10 +20,6 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 const host = '0.0.0.0';
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 app.use(cors())
 
 
